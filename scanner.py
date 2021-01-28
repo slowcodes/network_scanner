48816d416579fe14faac301b54fa9554c407094f
Ezenna Charles <charleszenna@gmail.com>
2021-01-28 18:43:28 -0500

    working project demonstrating the use of list and dictionary

--- scanner.py
+++ scanner.py
@@ -12,12 +12,23 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # print(arp_request.summary())
     broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    print (arp_request_broadcast.summary())
+    # print (arp_request_broadcast.summary())
     arp_request_broadcast.show()
     # scapy.ls(scapy.ARP()) # reveals all the fields that can be set in the scapy.ARP Class
     # scapy.ls(scapy.Ether()) # reveals all the fields that can be set in the scapy.Ether Class
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print (unanswered.summary())
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+    # answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    client_list = []
+    print("IP \t\t\tMac Address \n-----------------------------------------------------------------------------")
+    for element in answered_list:
+        # print (element[1].show())
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+        print (element[1].psrc + "\t\t" + element[1].hwsrc)
+        print ("-----------------------------------------------------------------------------")
+    print (client_list)
+    # print (unanswered.summary())
 
 
 scan("10.0.2.1/24")
